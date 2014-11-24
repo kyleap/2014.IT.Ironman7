a0b7f5c2696c355f893ed57259b5f388d7e7a234
Kyle <kyle.shen616@gmail.com>
2014-11-24 10:15:36 +0800

    Final

---
+++ CMS.Web/Controllers/LoginController.cs
@@ -0,0 +1,73 @@
+﻿using CMS.BLL.Services;
+using CMS.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CMS.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            LoginViewModel model = new LoginViewModel();
+            return View(model);
+        }
+
+        [HttpPost, ActionName("UserLogin")]
+        public ActionResult LoginForCollege(LoginViewModel model)
+        {
+            // 實務上可能驗證表單.驗證該使用者是否存在.寫在BLL層.Web層只要知道是否登入成功
+            var UserID = LoginService.CheckUser(model);
+            // 登入成功，寫入FormsAuthentication
+            SetLogin(UserID, "Admin");
+            return RedirectToAction("Index", "Home");
+        }
+
+        /// <summary>寫入FormsAuthentication登入資訊</summary>
+        /// <param name="UserID"></param>
+        /// <param name="Role"></param>
+        private void SetLogin(int UserID, string Role)
+        {
+            var now = DateTime.Now;
+            var ticket = new FormsAuthenticationTicket(
+                version: 1,
+                name: UserID.ToString(),
+                issueDate: now,
+                expiration: now.AddHours(12),
+                isPersistent: true,
+                userData: Role + ",",
+                cookiePath: FormsAuthentication.FormsCookiePath);
+
+            var encryptedTicket = FormsAuthentication.Encrypt(ticket);
+            var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+            Response.Cookies.Add(cookie);
+        }
+
+        /// <summary>登出頁面</summary>
+        /// <returns></returns>
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+
+            //清除所有的 session
+            Session.RemoveAll();
+
+            //建立一個同名的 Cookie 來覆蓋原本的 Cookie
+            HttpCookie cookie1 = new HttpCookie(FormsAuthentication.FormsCookieName, "");
+            cookie1.Expires = DateTime.Now.AddYears(-1);
+            Response.Cookies.Add(cookie1);
+
+            //建立 ASP.NET 的 Session Cookie 同樣是為了覆蓋
+            HttpCookie cookie2 = new HttpCookie("ASP.NET_SessionId", "");
+            cookie2.Expires = DateTime.Now.AddYears(-1);
+            Response.Cookies.Add(cookie2);
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
---
+++ CMS.Web/Models/PageDynamicNodeProvider.cs
@@ -0,0 +1,40 @@
+﻿using CMS.BLL.Services;
+using MvcSiteMapProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Web.Models
+{
+    public class PageDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodes)
+        {
+            var returnValue = new List<DynamicNode>();
+            
+            // 向BLL層取得選單
+            foreach (var item in PermissionService.GetMenu())
+            {
+                DynamicNode node = new DynamicNode();
+                // 選單名稱
+                node.Title = item.Name; 
+                // 有無父類別，沒有的話則傳空字串
+                node.ParentKey = item.ParentID == 0 ? "" : item.ParentID.ToString();
+                // 唯一值
+                node.Key = item.MenuID.ToString();
+                // MVC的View
+                node.Action = item.Action;
+                // MVC的Controller
+                node.Controller = item.Controller;
+                // 選單所分配的腳色，逗號分隔
+                node.Roles = item.Roles.Split(',').Where(c => !string.IsNullOrEmpty(c)).ToList();
+                // 
+                node.RouteValues.Add("id", item.MenuID);
+                returnValue.Add(node);
+            }
+            // Return
+            return returnValue;
+        }
+    }
+}
